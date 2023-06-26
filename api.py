60902bf8847c1737f5c61b77582e48ed62e4a70a
Jeff Desch <jeff.desch@gmail.com>
2023-06-26 14:17:17 -0500

    Rearranged order of file operations

--- api.py
+++ api.py
@@ -17,17 +17,17 @@ if __name__ == '__main__':
         endpoint_input={"text": """Hello? Testing 1, 2, 3..."""}
     )
 
+    audio_path = os.path.join(".", "audio")
+    if not os.path.exists(audio_path):
+        os.makedirs(audio_path)
+
+    with open(os.path.join(audio_path, "log.txt"), "w") as file:
+        json.dump(run_request, file)
+
     encoded_audio = run_request.get("output").get("data", None)
 
     if encoded_audio is not None:
         audio = base64.b64decode(encoded_audio)
-
-        audio_path = os.path.join(".", "audio")
-        if not os.path.exists(audio_path):
-            os.makedirs(audio_path)
-
-        with open(os.path.join(audio_path, "log.txt"), "w") as file:
-            json.dump(run_request, file)
 
         save(audio, os.path.join(audio_path, "voice.mp3"))
 
