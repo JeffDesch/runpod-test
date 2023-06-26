2bdea1901de17060201dd6e0fa31304c4940a297
Jeff Desch <jeff.desch@gmail.com>
2023-06-26 14:08:06 -0500

    Fixed audio encoding/serialization and added log file

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-import os
 import json
+import os
+import base64
 
 import runpod
 from dotenv import load_dotenv
@@ -16,18 +17,17 @@ if __name__ == '__main__':
         endpoint_input={"text": """Hello? Testing 1, 2, 3..."""}
     )
 
-    if run_request is not None:
-        with open("log.txt", "w") as file:
+    encoded_audio = run_request.get("output").get("data", None)
+
+    if encoded_audio is not None:
+        audio = base64.b64decode(encoded_audio)
+
+        audio_path = os.path.join(".", "audio")
+        if not os.path.exists(audio_path):
+            os.makedirs(audio_path)
+
+        with open(os.path.join(audio_path, "log.txt"), "w") as file:
             json.dump(run_request, file)
 
-        output = run_request.get("data", None)
-
-        if output is not None:
-            audio = output
-
-            audio_path = os.path.join(".", "audio")
-            if not os.path.exists(audio_path):
-                os.makedirs(audio_path)
-
-            save(audio, os.path.join(audio_path, "voice.mp3"))
+        save(audio, os.path.join(audio_path, "voice.mp3"))
 
--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
+import base64
 import runpod
 from elevenlabs import set_api_key, generate
 
@@ -8,13 +9,22 @@ set_api_key(os.getenv("XI_API_KEY"))
 
 def handler(event):
 
+    input_data = event.get("input", None)
+    if input_data is not None:
+        text = input_data.get("text")
+    else:
+        text = "Test text is missing!"
+
     audio = generate(
-        text=event.get("text"),
+        text=text,
         voice="Rachel",
         model="eleven_monolingual_v1",
     )
 
-    return {"refresh_worker": True, "data": audio}
+    encoded_audio = base64.b64encode(audio)
+    encoded_audio = str(encoded_audio, 'ascii')
+
+    return {"refresh_worker": True, "data": encoded_audio}
 
 
 runpod.serverless.start({"handler": handler})
